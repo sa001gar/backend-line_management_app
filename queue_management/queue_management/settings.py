ac16f7312547a28e8ec67431cb091f92dccf80a3
sa001gar <kundusagar233@gmail.com>
2024-12-09 01:05:13 +0530

    update

--- queue_management/queue_management/settings.py
+++ queue_management/queue_management/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Define the STATIC_ROOT path
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
